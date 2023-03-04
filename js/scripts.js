7fd3a80c93667714517c0fe9e38784e080f6da60
Sem G <sem.gebresilassie@sitedrive.com>
2023-03-04 17:31:40 +0200

    chore: few steps to final

---
+++ js/scripts.js
@@ -0,0 +1,162 @@
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+//
+// Scripts
+// 
+
+window.addEventListener('DOMContentLoaded', event => {
+
+      const images = new Array(3);
+
+      let currentIndex = 0;
+      const glitchImages = document.querySelectorAll(".glitch-image");
+
+      function updateCarousel() {
+        glitchImages.forEach((image, index) => {
+          if (index === currentIndex) {
+            image.classList.add("current");
+          } else {
+            image.classList.remove("current");
+          }
+        });
+      }
+
+        function startCarousel() {
+          setInterval(() => {
+            currentIndex = (currentIndex + 1) % images.length;
+            updateCarousel();
+          }, 15000);
+        }
+
+      startCarousel();
+
+
+
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        const brandLogo = document.body.querySelector('.brand-logo');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+            brandLogo.classList.remove('active')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+            brandLogo.classList.add('active')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+          // Activate SimpleLightbox plugin for portfolio items
+          new SimpleLightbox({
+              elements: '#portfolio a.portfolio-box img'
+          });
+
+    const canvas = document.createElement('canvas');
+    const container = document.getElementById('canvas-container');
+    container.appendChild(canvas);
+
+    const renderer = new THREE.WebGLRenderer({
+      canvas: canvas,
+      antialias: true,
+      alpha: true
+    });
+
+
+    const scene = new THREE.Scene();
+    const geometry = new THREE.BoxGeometry(1, 1, 1);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const mesh = new THREE.Mesh(geometry, material);
+    scene.add(mesh);
+
+
+
+});
+
+const modal = document.getElementById('modal');
+const fullImage = document.getElementById('full-image');
+
+const thumbnails = document.querySelectorAll('.gallery a');
+
+// / set cookie expiration time to the past
+var expires = new Date(0).toUTCString();
+
+// set cookie value and SameSite attribute
+document.cookie = "mycookie=myvalue; expires=" + expires + "; SameSite=None";
+
+// Initialize a variable to keep track of the current image index
+let currentImageIndex = 0;
+
+// Loop through the thumbnails and add a click event listener
+thumbnails.forEach((thumbnail, index) => {
+  thumbnail.addEventListener('click', function(event) {
+    event.preventDefault();
+    const imageSrc = this.getAttribute('data-image');
+    fullImage.src = imageSrc;
+    modal.style.display = 'block';
+    currentImageIndex = index;
+    document.body.style.overflow = 'hidden';
+  });
+});
+
+// When the user clicks on the "Next" button, show the next image
+const nextBtn = document.querySelector('.next');
+nextBtn.addEventListener('click', function() {
+  currentImageIndex = (currentImageIndex + 1) % thumbnails.length;
+  const nextImageSrc = thumbnails[currentImageIndex].getAttribute('data-image');
+  fullImage.src = nextImageSrc;
+});
+
+// When the user clicks on the "Previous" button, show the previous image
+const prevBtn = document.querySelector('.prev');
+prevBtn.addEventListener('click', function() {
+  currentImageIndex = (currentImageIndex - 1 + thumbnails.length) % thumbnails.length;
+  const prevImageSrc = thumbnails[currentImageIndex].getAttribute('data-image');
+  fullImage.src = prevImageSrc;
+});
+
+
+
+// close modal when user clicks outside of it
+const closeBtn = document.querySelector('.close');
+window.onclick = function(event) {
+  if (event.target == modal || event.target == closeBtn) {
+    modal.style.display = 'none';
+    document.body.style.overflow = 'auto';
+  }
+};
+
