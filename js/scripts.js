205f3b355827475bcf64a9f1481f233e75f05fe5
semosem <sem.gebresilassie@gmail.com>
2024-02-13 16:25:33 +0200

    add 2024-event-1 flyer

--- js/scripts.js
+++ js/scripts.js
@@ -173,7 +173,7 @@ window.onclick = function(event) {
 };
 
 // Set the date we're counting down to
-var countDownDate = new Date("Oct 4, 2023 17:30:00").getTime();
+var countDownDate = new Date("Feb 15, 2024 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
