e6629c116671a79ee0762c6603085dfbbc6c5259
Sem G. <sem.gebresilassie@gmail.com>
2025-03-10 10:30:22 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -162,7 +162,7 @@ window.onclick = function (event) {
 };
 
 // Set the date we're counting down to
-var countDownDate = new Date("Mar 6, 2025 17:30:00").getTime();
+var countDownDate = new Date("Mar 19, 2025 17:00:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
